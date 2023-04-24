57088650f9d73ca45f24a2f1303c1768fa7dd880
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-24 13:23:16 +0300

    Add structure with styles

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="form-container">
-      <form className="register-form">
+      <form class="register-form">
+        {/* Uncomment the next line to show the success message */}
+        {/* <div class="success-message">Success! Thank you for registering</div> */}
         <input
-        className="form-field"
-        placeholder="First Name"
-        name="firstName"
-        ></input>
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="first-name-error">Please enter a first name</span> */}
         <input
-        className="form-field"
-        placeholder="Last Name"
-        name="lastName"
-        ></input>
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="last-name-error">Please enter a last name</span> */}
         <input
-        className="form-field"
-        placeholder="Email"
-        name="email"
-        ></input>
-        <button
-        className="form-field"
-        type="submit"
-        >Register</button>
+          id="email"
+          class="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="email-error">Please enter an email address</span> */}
+        <button class="form-field" type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
